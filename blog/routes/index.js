e7cdb7e075b92d3d871e09b832cd30954c9b31d3
hassan <hchoudhury20@hotmail.com>
2023-09-06 22:12:24 +0100

    add function to verify jwt token

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -8,10 +8,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get("/post",(req,res) => {
+router.get("/post",verifyToken,(req,res) => {
   res.render("form",{title:"form"})
 })
 router.post("/post",post.createpost)
+
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // Check if bearer is undefined
+  if(typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+
+}
 
 
 module.exports = router;
