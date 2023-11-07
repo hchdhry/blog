6d2770319c17d35c4260a8dba7b6a5f3a10ad361
hassan <hchoudhury20@hotmail.com>
2023-11-07 23:42:01 +0000

    add error handling to post authoring functionality

--- front-end/src/components/App.jsx
+++ front-end/src/components/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LogIn from './login';
 import Home from './home';
+import Author from "./author"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<LogIn />} />
         <Route path="/" element={<Home />} />
+        <Route path="/author"element={<Author/>}/>
       </Routes>
     </Router>
   );
--- front-end/src/components/author.jsx
+++ front-end/src/components/author.jsx
@@ -1,37 +1,61 @@
 import { useState } from "react";
 import "../app.css";
+import { set } from "mongoose";
 
+const Author = () => {
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const [status,setStatus] = useState("");
+  const [error,setError] = useState("");
 
-const Author= ()=>{
-    const [Title,setTitle] = useState("");
-    const [Message,setMessage] = useState("");
-    return(
-        <div className="Author">
-            <h1>make post</h1>
-            <div>
-                <label htmlFor="Title">Title</label>
-                <input
-                type="text"
-                id = "title"
-                value={Title}
-                onChange={(e)=>{setTitle(e.target.value)}}
-                />
-            </div>
-            <div>
-                <label htmlFor="message">Your message</label>
-                <input
-                type="text"
-                id="message"
-                value={Message}
-                onChange={(e)=>{setMessage(e.target.value)}}
-                />
-            </div>
-      
+  async function handleSubmit(event) {
+    event.preventDefault(); 
+    
+    try {
+      const req = await fetch("http://localhost:3000/post", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title: title, text: message })
+      });
+  
+      if (req.ok) {
+       setStatus("post created")
+      } else {
+        setError("error")
+      }
+    } catch (err) {
+        setError("error")
+    }
+  }
+  
 
-
+  return (
+    <div className="Author">
+        {status&&<p style={{ color: 'green' }}>{status}</p>}
+        {error&&<p style={{ color: 'red' }}>{error}</p>}
+      <h1>Make Post</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
-    )
+        <div>
+          <label htmlFor="message">Your Message</label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-}
-
-export default Author
+export default Author;
